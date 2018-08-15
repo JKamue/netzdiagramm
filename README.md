fe476db82508a13dc0014176d2dc45f9a27da42e
JKamue <laptop@jkamue.de>
2018-08-15 14:09:12 +0200

    v.2

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# netzdiagramm
+# Netzdiagramm
 Dieses Script erstellt ein Netzdiagramm mit beliebig Größe und n-Ecken in einem canvas
 
 ALs Input gibt man...
 	...ein Array mit der jeweiligen Punktzahl zwischen 0 (nichts) und 10 (maximale Punktzahl)
 	...den X und Y abstand vom Canvas
 	...den Radius des n-Ecks
-	...die Anzahl der Ecken des n-Ecks
-	
-	
-Möchte man das n-Eck selber zeichnen oder benötigt nur die Punkte, so kann man diese aus den beiden Arrays xcoord und ycoord bekommen.
+		
+Changelog
+	*v.2
+		  *Die Anzahl der Ecken wird nun automatisch an die Anzahl an gegebenen Datenpunkten angepasst
+		  *Man Kann nun eine Skala ein und ausschalten
+		  *Man Kann nun eine Beschriftung ein und ausschalten
---
+++ diagram.js
@@ -0,0 +1,96 @@
+			function degrees_to_radians(degrees){
+			  return degrees * (Math.PI/180);
+			}
+			
+			function draw_path(ctx,sizex,sizey,xcoord,ycoord,line){
+				xcoord.length
+				for (i = 0; i < xcoord.length; i++) {
+					ctx.beginPath();
+					ctx.lineWidth=line;
+					ctx.moveTo(sizex,sizey);
+					ctx.lineTo(xcoord[i],ycoord[i]);
+					if(i == corners-1){
+						ctx.lineTo(xcoord[0],ycoord[0]);
+					}else{
+						ctx.lineTo(xcoord[1*(i)+1],ycoord[1*(i)+1]);
+					}
+					ctx.stroke();
+				}
+			}
+			
+			function draw(){
+				
+				var degrees = 360/corners;
+				
+				var xcoord = [];
+				var ycoord = [];
+				
+				for (i = 0; i < corners; i++) {
+					var	a = i+1;
+					var x = Math.round(radius*Math.cos(degrees_to_radians(a*degrees))+1*(sizex));
+					var y = Math.round(radius*Math.sin(degrees_to_radians(a*degrees))+1*(sizey));
+					xcoord.push(x);
+					ycoord.push(y);
+				}
+				
+				var canvas = document.getElementById("myCanvas");
+				var ctx = canvas.getContext("2d");
+				
+				ctx.fillStyle = "white";
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
+				ctx.fill();
+				ctx.fillStyle = "black";
+				draw_path(ctx,sizex,sizey,xcoord,ycoord,2);
+				
+				if(activate_skala == true){
+					for (f = 1; f < 10; f++) {
+						var skalax = [];
+						var skalay = [];
+						var size = radius/(10/f);
+						for (i = 0; i < corners; i++) {
+							var	a = i+1;
+							var x = Math.round(size*Math.cos(degrees_to_radians(a*degrees))+1*(sizex));
+							var y = Math.round(size*Math.sin(degrees_to_radians(a*degrees))+1*(sizey));
+							skalax.push(x);
+							skalay.push(y);
+						}
+						draw_path(ctx,sizex,sizey,skalax,skalay,1);
+					}
+				}
+				
+				var scorex = [];
+				var scorey = [];
+				for (i = 0; i < corners; i++) {
+					var	a = i+1;
+					var size = radius/(10/score[i]);
+					var x = Math.round(size*Math.cos(degrees_to_radians(a*degrees))+1*(sizex));
+					var y = Math.round(size*Math.sin(degrees_to_radians(a*degrees))+1*(sizey));
+					scorex.push(x);
+					scorey.push(y);
+				}	
+
+				for (i = 0; i < corners; i++) {
+					ctx.beginPath();
+					ctx.strokeStyle = '#ff0000';
+					ctx.moveTo(scorex[i],scorey[i]);
+					if(i == corners-1){
+						ctx.lineTo(scorex[0],scorey[0]);
+					}else{
+						ctx.lineTo(scorex[1*(i)+1],scorey[1*(i)+1]);
+					}
+					ctx.stroke();
+				}
+				
+				if(activate_text == true){
+					ctx.font = font_size + "px Arial";
+					ctx.strokeStyle = 'black';
+					for (i = 0; i < corners; i++) {
+						var	a = i+1;
+						var size = radius/(10/11);
+						var x = Math.round(size*Math.cos(degrees_to_radians(a*degrees))+1*(sizex))-1*(font_size/3);
+						var y = Math.round(size*Math.sin(degrees_to_radians(a*degrees))+1*(sizey))+1*(font_size/2);
+						ctx.fillText(a,x,y);
+					}	
+				}
+				
+			}
